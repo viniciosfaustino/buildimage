15cc18033ad35544b05f9d8b9ad2432145139a05
viniciosfaustino <vinicioskf@gmail.com>
2020-03-08 16:59:56 -0400

    iniciando repositório

--- buildimage.c
+++ buildimage.c
@@ -19,8 +19,25 @@
 
 /* Reads in an executable file in ELF format*/
 Elf32_Phdr * read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr){
- 
-  return NULL;
+  Elf32_Phdr* phdr = malloc(sizeof(Elf32_Phdr));
+  *execfile = fopen((const char*)filename, "rb");
+  if (*execfile){
+
+    size_t step = fread(*ehdr, 1, sizeof(**ehdr), *execfile);
+    // fseek(*execfile, step, SEEK_SET);
+    printf("AI MEU PIRUUUUUUUUUUUUUUUUUU\n");
+    step = fread(phdr, 1, sizeof(*phdr), *execfile);
+    
+    fclose(*execfile);
+    printf("novinha senta de vagaf\n");
+    return phdr;
+
+  }
+  else
+  {
+    return NULL;
+  }
+  
 }
 
 /* Writes the bootblock to the image file */
@@ -69,15 +86,18 @@ int main(int argc, char **argv){
   FILE *kernelfile, *bootfile,*imagefile;  //file pointers for bootblock,kernel and image
   Elf32_Ehdr *boot_header = malloc(sizeof(Elf32_Ehdr));//bootblock ELF header
   Elf32_Ehdr *kernel_header = malloc(sizeof(Elf32_Ehdr));//kernel ELF header
-
   Elf32_Phdr *boot_program_header; //bootblock ELF program header
   Elf32_Phdr *kernel_program_header; //kernel ELF program header
 
   /* build image file */
+  imagefile =  fopen(IMAGE_FILE, "w");  
 
   /* read executable bootblock file */  
-
-  /* write bootblock */  
+  boot_program_header = read_exec_file(&bootfile, argv[1], &boot_header);
+  printf("a sua mae foi trabalhar na zona\n");
+  /* write bootblock */    
+  fwrite(boot_header, 1, sizeof(*boot_header), imagefile);
+  fwrite(boot_program_header, 1, sizeof(*boot_program_header), imagefile);
 
   /* read executable kernel file */
 
@@ -88,8 +108,8 @@ int main(int argc, char **argv){
   /* check for  --extended option */
   if(!strncmp(argv[1],"--extended",11)){
 	/* print info */
-  }
-  
+  }  
+  fclose(imagefile);
   return 0;
 } // ends main()
 
